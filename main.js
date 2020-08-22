242e2664c65a742eaaf2464329129dab4eca4b3a
EyePatch <007abeershariyar@gmail.com>
2020-08-23 03:55:51 +0600

    update

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
 
 const bot = new Discord.Client();
 
-const token = "";
-
 bot.once("ready", () => {
 	console.log("Bot is Online!! Happy Coding !");
+});
+
+bot.on("message", (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === "ping") {
+		message.channel.send("Pong!!!");
+	} else if (command === "you") {
+		message.channel.send("I am a bot Linda");
+	}
 });
 
 bot.login(token);
